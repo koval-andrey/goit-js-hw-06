0440bc7049d947bf44952926993358cb689c71c2
koval-andrey <70697160+koval-andrey@users.noreply.github.com>
2021-01-17 23:26:08 +0200

    16.01.2021
    9 заданий

--- js/task.js
+++ js/task.js
@@ -69,21 +69,24 @@ console.log(calculateTotalBalance(users)); // 20916
 
 const getUsersWithFriend = (users, friendName) => 
   // твой код
-  users.filter(user => user.friends === friendName)
+  users.filter(user => user.friends.some(friend => friend === friendName))
   .map(user => user.name);
 
 console.log(getUsersWithFriend(users, 'Briana Decker')); // [ 'Sharlene Bush', 'Sheree Anthony' ]
 console.log(getUsersWithFriend(users, 'Goldie Gentry')); // [ 'Elma Head', 'Sheree Anthony' ]
 /*Задание 9
-Массив имен (поле name) людей, отсортированных в зависимости от количества их друзей (поле friends)
+Массив имен (поле name) людей, отсортированных в зависимости от количества их друзей (поле friends)*/
 
-const getNamesSortedByFriendsCount = users => {
+const getNamesSortedByFriendsCount = users =>
   // твой код
-};
+  users
+  .slice()
+  .sort((prev, next) => prev.friends.length - next.friends.length)
+  .map(user => user.name);
 
 console.log(getNamesSortedByFriendsCount(users));
 // [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
-Задание 10
+/*Задание 10
 Получить массив всех умений всех пользователей (поле skills), при этом не должно быть повторяющихся умений и они должны быть отсортированы в алфавитном порядке.
 
 const getSortedUniqueSkills = users => {
